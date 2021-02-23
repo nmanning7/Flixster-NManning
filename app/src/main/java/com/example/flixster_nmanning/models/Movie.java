dda060f43d74600dc0b7145e5d539c0392aa3397
nmanning7 <nmanning117@rams.wssu.edu>
2021-02-22 19:14:11 -0500

    Updated files

--- app/src/main/java/com/example/flixster_nmanning/models/Movie.java
+++ app/src/main/java/com/example/flixster_nmanning/models/Movie.java
@@ -3,22 +3,29 @@ package com.example.flixster_nmanning.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
+    int movieId;
     String  posterPath;
     String title;
     String overview;
     String  backdropPath;
+    double rating;
 
+    public Movie(){ }
 
     public Movie(JSONObject jsonObject) throws JSONException {
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
         backdropPath = jsonObject.getString("backdrop_path");
+        rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJSONArray(JSONArray movieJsonArray) throws JSONException {
@@ -45,6 +52,11 @@ public class Movie {
         return overview;
     }
 
-
+    public double getRating() {
+        return rating;
+    }
+    public int getMovieId(){
+        return movieId;
+    }
 }
 
